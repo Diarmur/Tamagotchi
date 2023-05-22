4347adc9708e2d51c25c88ac8bdef392f15d7d1b
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-05-22 16:35:42 +0200

    set tamagochi

---
+++ project-tamagotchi/src/main/java/com/ynov/tamagochi/Tamagochi.java
@@ -0,0 +1,15 @@
+package com.ynov.tamagochi;
+
+public class Tamagochi {
+    int bonheur = 15;
+    int age = 0;
+    String StagesOfLife = "oeuf";
+    int faim = 100; //en pourcent  
+
+
+    // pour chaque uniter de temps passer
+    void update(){
+
+    }
+    
+}
