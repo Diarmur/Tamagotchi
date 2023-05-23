94230bef77842f362b53b80fd751a53d30908d83
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-05-23 14:58:05 +0200

    set update

--- project-tamagotchi/src/main/java/com/ynov/tamagochi/Tamagochi.java
+++ project-tamagotchi/src/main/java/com/ynov/tamagochi/Tamagochi.java
@@ -4,13 +4,27 @@ public class Tamagochi {
     int happiness = 15;
     int age = 0;
     String StagesOfLife = "egg";
-    int hunger = 100; //en pourcent 
-    boolean eat = false; 
+    int hunger = 0; 
+    boolean allReadyEat = false;
+    boolean dirty = false; 
 
 
     // pour chaque uniter de temps passer
     void update(){
-
+    if(this.allReadyEat == true){
+        this.allReadyEat = false;
+        this.dirty = true;
+        this.hunger = 0;
+    }else{
+        this.hunger ++;
+        this.happiness -= this.hunger *5;
+        }
+    if(this.dirty == true){
+        this.happiness -=3;
     }
+    }
+
+
+
     
 }
