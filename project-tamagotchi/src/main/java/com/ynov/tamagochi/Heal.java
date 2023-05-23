d2bbe9cafb164c657a13284412055698eee7d654
matiasbellaud <bellaud.matias@gmail.com>
2023-05-23 16:42:20 +0200

    clean and sick tamagotchi

---
+++ project-tamagotchi/src/main/java/com/ynov/tamagochi/Heal.java
@@ -0,0 +1,32 @@
+package com.ynov.tamagochi;
+
+public class Heal {
+    Tamagotchi tamagotchi;
+    int ageSick;
+
+    public Heal(Tamagotchi tamagotchi) {
+        this.tamagotchi = tamagotchi;
+    }
+
+    public void Sick() {
+        if (this.tamagotchi.StagesOfLife.equals("elder")) {
+            int chanceOfSickness = (int) (Math.random() * 3);
+            if (chanceOfSickness == 0) {
+                this.tamagotchi.sick = true;
+                this.ageSick = this.tamagotchi.age;
+            }
+        }
+    }
+
+    public void HealTamagotchi() {
+        if (this.tamagotchi.sick && this.tamagotchi.age > this.ageSick) {
+            this.tamagotchi.sick = false;
+        }
+    }
+
+    public void DeathBySick() {
+        if (this.tamagotchi.sick && this.tamagotchi.age > this.ageSick) {
+            this.tamagotchi.StagesOfLife = "dead";
+        }
+    }
+}
